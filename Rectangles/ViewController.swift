45d2171bfa5ad81b4d136831e8f50e4146a97d03
Javi <javi_castillo18@hotmail.com>
2021-03-29 09:36:43 +0200

    First commit

---
+++ Rectangles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rectangles
+//
+//  Created by Javi Castillo Risco on 29/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
