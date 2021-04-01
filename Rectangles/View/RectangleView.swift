3909e1aa71743dc15296197da638b83ae9fcd23f
Javi <javi_castillo18@hotmail.com>
2021-04-01 10:27:59 +0200

    Add actions in rectangles

--- Rectangles/View/RectangleView.swift
+++ Rectangles/View/RectangleView.swift
@@ -19,6 +19,40 @@ class RectangleView: UIView {
 
     private var originalPosition: CGPoint?
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        prepareRectangle()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        prepareRectangle()
+    }
+
+    private func prepareRectangle() {
+        backgroundColor = randomColor()
+
+        //Add actions
+        let tap = UITapGestureRecognizer()
+        tap.addTarget(self, action: #selector(simpleTapDone))
+        addGestureRecognizer(tap)
+
+        let doubleTap = UITapGestureRecognizer()
+        doubleTap.addTarget(self, action: #selector(doubleTapDone))
+        doubleTap.numberOfTapsRequired = 2
+        addGestureRecognizer(doubleTap)
+    }
+
+    @objc
+    private func simpleTapDone(recognizer: UITapGestureRecognizer) {
+
+    }
+
+    @objc
+    private func doubleTapDone(recognizer: UITapGestureRecognizer) {
+
+    }
+
     func setLocation(_ location: CGPoint) {
         originalPosition = location
         let MAXSize: CGFloat = 150
@@ -29,6 +63,11 @@ class RectangleView: UIView {
         let newX = location.x - newWidth/2
         let newY = location.y - newHeight/2
         frame = CGRect(x: newX, y: newY, width: newWidth, height: newHeight)
+    }
+
+    private func randomColor() -> UIColor {
+        let hue: CGFloat = CGFloat(arc4random() % 100) / 100.0
+        return UIColor(hue: hue, saturation: 1, brightness: 1, alpha: 0.75)
     }
 
 }
